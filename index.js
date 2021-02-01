500b50d7276454514f281fabebd50315da52e246
Michael Fuget <michaelfuget@gmail.com>
2021-02-01 09:16:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 append.push("Odie")
 prepend.unshift("Odie")
-removeLast = removelast.pop()
-removeFirst=removeFirst.shift()
+removelast.pop()
+removeFirst.shift()
